c2824a44b536df72d2e37bf521de729f99a6043a
EvaMaleckova <eva.maleckova@hhu.de>
2026-07-27 11:15:04 +0200

    Updated Economist css

--- src/pages/EconomistPage.jsx
+++ src/pages/EconomistPage.jsx
@@ -1,6 +1,8 @@
 import EconomistPlot from "../visualizations/02_TheEconomistPlot/economistPlot";
 import economistData from "../visualizations/02_TheEconomistPlot/economistData";
 
+import "../visualizations/02_TheEconomistPlot/economistPlot.css";
+
 export default function economistPage() {
   return (
     <div>
