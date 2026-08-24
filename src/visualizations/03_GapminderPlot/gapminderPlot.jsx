e234ad33d3a26a4757672a04ef2158b7d8817f6c
EvaMaleckova <eva.maleckova@hhu.de>
2026-08-24 13:02:45 +0200

    Continent legend

--- src/visualizations/03_GapminderPlot/gapminderPlot.jsx
+++ src/visualizations/03_GapminderPlot/gapminderPlot.jsx
@@ -148,6 +148,39 @@ export default function GapminderPlot({ data }) {
             pixelsPerTick={50}
             label={"Life expectancy"}
           />
+          {/* Continent legend */}
+          <text
+            x={xScale(25000)}
+            y={yScale(minLifeExp * 1.35)}
+            textAnchor="left"
+            alignmentBaseline="middle"
+            fill="black"
+            fontSize="9px"
+          >
+            Continents
+          </text>
+          {continents.map((continent, i) => (
+            <g
+              key={i}
+              transform={`translate(${xScale(25000)}, ${yScale(minLifeExp * 1.25) + i * 15})`}
+            >
+              <circle
+                r={5}
+                fill={colorScale(continent)}
+                fillOpacity={0.5}
+                stroke={colorScale(continent)}
+              />
+              <text
+                x={15}
+                y={0}
+                alignment="left"
+                alignmentBaseline="middle"
+                fontSize="10px"
+              >
+                {continent}
+              </text>
+            </g>
+          ))}
         </g>
       </svg>
     </div>
