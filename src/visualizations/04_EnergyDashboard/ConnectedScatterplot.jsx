608c71311a158d1ca8b91fcab3639e16ba9a0eae
EvaMaleckova <eva.maleckova@hhu.de>
2026-09-14 11:32:54 +0200

    Functional connected scatter

--- src/visualizations/04_EnergyDashboard/ConnectedScatterplot.jsx
+++ src/visualizations/04_EnergyDashboard/ConnectedScatterplot.jsx
@@ -8,18 +8,20 @@ const MARGIN = {
 };
 
 const width = 600;
-const height = 400;
+const height = 600;
 
 export function ConnectedScatterplot({ data, width, height }) {
+  // Conversion: numerical year to date
+  data = data.map((d) => ({
+    ...d,
+    year: new Date(d.year, 1, 1), // Jan 1st of each year
+  }));
   const boundsWidth = width - MARGIN.left - MARGIN.right;
   const boundsHeight = height - MARGIN.top - MARGIN.bottom;
 
-  // x axis
+  // x axis - for time
   const [xMin, xMax] = d3.extent(data, (d) => d.year);
-  const xScale = d3
-    .scaleLinear()
-    .domain([0, xMax || 0])
-    .range([0, boundsWidth]);
+  const xScale = d3.scaleTime().domain([xMin, xMax]).range([0, boundsWidth]);
   // y axis
   const [yMin, yMax] = d3.extent(data, (d) => d.primary_energy);
   const yScale = d3
@@ -52,7 +54,7 @@ export function ConnectedScatterplot({ data, width, height }) {
   return (
     <svg width={width} height={height}>
       <g transform={`translate(${MARGIN.left},${MARGIN.top})`}>
-        <path d={linePath} fill="none" stroke="black" />
+        <path d={linePath} fill="none" stroke="#cb1dd1" />
         {allCircles}
       </g>
     </svg>
--- src/visualizations/04_EnergyDashboard/EnergyDashboard.jsx
+++ src/visualizations/04_EnergyDashboard/EnergyDashboard.jsx
@@ -3,10 +3,20 @@ import * as d3 from "d3";
 import { ConnectedScatterplot } from "./ConnectedScatterplot";
 import energyData from "./energyData";
 
+const worldPrimaryEnergy = energyData.filter(
+  (global) => global.country === "World",
+);
+
+console.log(worldPrimaryEnergy);
+
 export default function EnergyDashboard() {
   return (
-    <svg width={600} height={400}>
-      <ConnectedScatterplot data={energyData} width={600} height={400} />
+    <svg width={600} height={600}>
+      <ConnectedScatterplot
+        data={worldPrimaryEnergy}
+        width={600}
+        height={600}
+      />
     </svg>
   );
 }
